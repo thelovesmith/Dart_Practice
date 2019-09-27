cba1eb5b2a90800a5fb642555a552b22cc5745a7
thelovesmith <averydante9@gmail.com>
2019-09-27 10:32:05 -0400

    did my first pure Dart function

--- hello.dart
+++ hello.dart
@@ -4,4 +4,20 @@ void main() {
   const road = "and goodbye";
   var travel = car + road; 
   print(travel); 
+  
+  final int number = 9;
+
+isGreater(num){
+  if(num < 10){
+    return "success";
+  } else if(num == 10){
+    return "is 10!";
+
+  } else {
+    return "oops!!";
+  }
+}
+print(isGreater(number));
+print(isGreater(100));
+
 }
