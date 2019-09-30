87a205faceebcbff9e469e54a4243b08b74d875d
thelovesmith <averydante9@gmail.com>
2019-09-30 13:34:30 -0400

    created avg funciton

---
+++ auditAvg.dart
@@ -0,0 +1,14 @@
+// Function to find averages for Web Performance Audits/ 
+
+void main(){
+  print("is working");
+isAvg(a, b, c, d, e){
+var average = (a + b + c + d + e) / 5;
+print(average);
+}
+isAvg(58, 60, 89, 45, 50);
+isAvg(19, 100, 86, 92, 50);
+isAvg(81, 86, 71, 91, 50);
+isAvg(16, 80, 71, 100, 50);
+isAvg(16, 80, 71, 100, 50);
+}
