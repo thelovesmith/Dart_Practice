179194c5551993f31dbe886560188dede33fbcdd
thelovesmith <averydante9@gmail.com>
2019-10-02 10:01:37 -0400

    triend the counting sheep exercise

---
+++ countSheep.dart
@@ -0,0 +1,14 @@
+void main() {
+  String countSheep(numb) {
+    var sheeps = " sheep...";
+    var x = numb.toString();
+    for(var i = numb; i > 0; i++){
+      
+    }
+    
+    var sleep = x + sheeps;
+    return sleep;
+  }
+
+  print(countSheep(4)); 
+}
